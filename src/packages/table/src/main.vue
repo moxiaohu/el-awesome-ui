f04a1b021f0fcc2fa3b573028a4c868afd96bda0
Xiaohu MO <mo.x@sfeir.com>
2019-05-27 16:25:27 +0200

    no message

--- src/packages/table/src/main.vue
+++ src/packages/table/src/main.vue
@@ -29,7 +29,7 @@ export default {
   name: "ela-table",
   props: {
     dataList: { type: Array, required: false, default: null },
-    structure: { type: Array, required: true }
+    structure: { type: Array, required: false, default: [] }
   },
   watch: {
     // To make code simple, show key in data object if it is not mentioned in this.structure, 
